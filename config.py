06d62597b85c723a9c0227d7bf17325d005f152a
Ronit-pro <85551217+Ronit-pro@users.noreply.github.com>
2024-08-31 20:20:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7318307196:AAHRZmtcaTP4W926cMvszDYVl_3lsR0vtHI")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("RONIT_IN")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7246521618")
 
 
 
